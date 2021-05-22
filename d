2e32ceabe5e8e0857067da797bacb947bb024fa2
federicorojas11 <federico.rojas.121@gmail.com>
2021-05-22 00:24:46 -0300

    initial commit - routes

---
+++ d
@@ -0,0 +1,11 @@
+[33mcommit 48f271c5a2007fb57c4ebf500ffb9edaedce6fff[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: federicorojas11 <federico.rojas.121@gmail.com>
+Date:   Fri May 21 21:25:29 2021 -0300
+
+    initial commit - rutas y archivos creados
+
+[33mcommit 60a7df344ee8935ba601474aea7b7e6a2c25f81d[m
+Author: federicorojas11 <federico.rojas.121@gmail.com>
+Date:   Fri May 21 20:37:14 2021 -0300
+
+    initial commit
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
